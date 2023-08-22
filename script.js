f39f598c1923ff7564a75e543cb22de96e7b6878
sajiboye28 <59025639+sajiboye28@users.noreply.github.com>
2023-08-23 00:40:41 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,604 @@
+var arrayOfQuotes = [
+  {
+    "author": "Jim Rohn",
+    "quote": "Beware of what you become in pursuit of what you want.",
+  },
+  {
+    "author": "Maya Angelou",
+    "quote":
+      "I've learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.",
+  },
+  {
+    "author": "Albert Einstein",
+    "quote":
+      "Try not to become a man of success, but rather try to become a man of value.",
+  },
+  {
+    "author": "Nelson Mandela",
+    "quote":
+      "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+  },
+  {
+    "author": "Steve Jobs",
+    "quote":
+      "Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work. And the only way to do great work is to love what you do.",
+  },
+  {
+    "author": "Oprah Winfrey",
+    "quote":
+      "The biggest adventure you can ever take is to live the life of your dreams.",
+  },
+  {
+    "author": "Walt Disney",
+    "quote":
+      "All our dreams can come true, if we have the courage to pursue them.",
+  },
+  {
+    "author": "Mark Twain",
+    "quote": "The secret of getting ahead is getting started.",
+  },
+  {
+    "author": "Eleanor Roosevelt",
+    "quote":
+      "The future belongs to those who believe in the beauty of their dreams.",
+  },
+  {
+    "author": "Abraham Lincoln",
+    "quote":
+      "In the end, it's not the years in your life that count. It's the life in your years.",
+  },
+  {
+    "author": "Martin Luther King Jr.",
+    "quote":
+      "Darkness cannot drive out darkness; only light can do that. Hate cannot drive out hate; only love can do that.",
+  },
+  {
+    "author": "Mother Teresa",
+    "quote":
+      "Spread love everywhere you go. Let no one ever come to you without leaving happier.",
+  },
+  {
+    "author": "John F. Kennedy",
+    "quote":
+      "Ask not what your country can do for you, ask what you can do for your country.",
+  },
+  {
+    "author": "Mahatma Gandhi",
+    "quote": "Be the change that you wish to see in the world.",
+  },
+  {
+    "author": "Stephen Covey",
+    "quote":
+      "The key is not to prioritize what's on your schedule, but to schedule your priorities.",
+  },
+  {
+    "author": "Henry Ford",
+    "quote": "Whether you think you can or you think you can't, you're right.",
+  },
+  {
+    "author": "Theodore Roosevelt",
+    "quote": "Believe you can and you're halfway there.",
+  },
+  {
+    "author": "Zig Ziglar",
+    "quote":
+      "You don't have to be great to start, but you have to start to be great.",
+  },
+  {
+    "author": "J.K. Rowling",
+    "quote":
+      "It is our choices, Harry, that show what we truly are, far more than our abilities.",
+  },
+  {
+    "author": "Stephen Hawking",
+    "quote": "Intelligence is the ability to adapt to change.",
+  },
+  {
+    "author": "Thomas Edison",
+    "quote": "I have not failed. I've just found 10,000 ways that won't work.",
+  },
+  {
+    "author": "Benjamin Franklin",
+    "quote":
+      "Tell me and I forget. Teach me and I remember. Involve me and I learn.",
+  },
+  {
+    "author": "Winston Churchill",
+    "quote":
+      "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+  },
+  {
+    "author": "Ralph Waldo Emerson",
+    "quote":
+      "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.",
+  },
+  {
+    "author": "William Shakespeare",
+    "quote": "All the world's a stage, and all the men and women merely players.",
+  },
+  {
+    "author": "Leo Tolstoy",
+    "quote":
+      "Everyone thinks of changing the world, but no one thinks of changing himself.",
+  },
+  {
+    "author": "Friedrich Nietzsche",
+    "quote": "He who has a why to live can bear almost any how.",
+  },
+  {
+    "author": "George Bernard Shaw",
+    "quote":
+      "Life isn't about finding yourself. Life is about creating yourself.",
+  },
+  {
+    "author": "Pablo Picasso",
+    "quote":
+      "Every child is an artist. The problem is how to remain an artist once he grows up.",
+  },
+  {
+    "author": "Vincent Van Gogh",
+    "quote": "I am seeking, I am striving, I am in it with all my heart.",
+  },
+  {
+    "author": "C.S. Lewis",
+    "quote": "You are never too old to set another goal or to dream a new dream.",
+  },
+  {
+    "author": "J.R.R. Tolkien",
+    "quote":
+      "All we have to decide is what to do with the time that is given us.",
+  },
+  {
+    "author": "Ernest Hemingway",
+    "quote":
+      "The world breaks everyone, and afterward, some are strong at the broken places.",
+  },
+  {
+    "author": "Maya Angelou",
+    "quote":
+      "I've learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.",
+  },
+  {
+    "author": "Albert Einstein",
+    "quote":
+      "Try not to become a man of success, but rather try to become a man of value.",
+  },
+  {
+    "author": "Nelson Mandela",
+    "quote":
+      "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+  },
+  {
+    "author": "Steve Jobs",
+    "quote":
+      "Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work. And the only way to do great work is to love what you do.",
+  },
+  {
+    "author": "Oprah Winfrey",
+    "quote":
+      "The biggest adventure you can ever take is to live the life of your dreams.",
+  },
+  {
+    "author": "Walt Disney",
+    "quote":
+      "All our dreams can come true, if we have the courage to pursue them.",
+  },
+  {
+    "author": "Mark Twain",
+    "quote": "The secret of getting ahead is getting started.",
+  },
+  {
+    "author": "Eleanor Roosevelt",
+    "quote":
+      "The future belongs to those who believe in the beauty of their dreams.",
+  },
+  {
+    "author": "Abraham Lincoln",
+    "quote":
+      "In the end, it's not the years in your life that count. It's the life in your years.",
+  },
+  {
+    "author": "Martin Luther King Jr.",
+    "quote":
+      "Darkness cannot drive out darkness; only light can do that. Hate cannot drive out hate; only love can do that.",
+  },
+  {
+    "author": "Mother Teresa",
+    "quote":
+      "Spread love everywhere you go. Let no one ever come to you without leaving happier.",
+  },
+  {
+    "author": "John F. Kennedy",
+    "quote":
+      "Ask not what your country can do for you, ask what you can do for your country.",
+  },
+  {
+    "author": "Mahatma Gandhi",
+    "quote": "Be the change that you wish to see in the world.",
+  },
+  {
+    "author": "Stephen Covey",
+    "quote":
+      "The key is not to prioritize what's on your schedule, but to schedule your priorities.",
+  },
+  {
+    "author": "Henry Ford",
+    "quote": "Whether you think you can or you think you can't, you're right.",
+  },
+  {
+    "author": "Theodore Roosevelt",
+    "quote": "Believe you can and you're halfway there.",
+  },
+  {
+    "author": "Zig Ziglar",
+    "quote":
+      "You don't have to be great to start, but you have to start to be great.",
+  },
+  {
+    "author": "J.K. Rowling",
+    "quote":
+      "It is our choices, Harry, that show what we truly are, far more than our abilities.",
+  },
+  {
+    "author": "Stephen Hawking",
+    "quote": "Intelligence is the ability to adapt to change.",
+  },
+  {
+    "author": "Thomas Edison",
+    "quote": "I have not failed. I've just found 10,000 ways that won't work.",
+  },
+  {
+    "author": "Benjamin Franklin",
+    "quote":
+      "Tell me and I forget. Teach me and I remember. Involve me and I learn.",
+  },
+  {
+    "author": "Winston Churchill",
+    "quote":
+      "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+  },
+  {
+    "author": "Ralph Waldo Emerson",
+    "quote":
+      "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.",
+  },
+  {
+    "author": "William Shakespeare",
+    "quote": "All the world's a stage, and all the men and women merely players.",
+  },
+  {
+    "author": "Leo Tolstoy",
+    "quote":
+      "Everyone thinks of changing the world, but no one thinks of changing himself.",
+  },
+  {
+    "author": "Friedrich Nietzsche",
+    "quote": "He who has a why to live can bear almost any how.",
+  },
+  {
+    "author": "George Bernard Shaw",
+    "quote":
+      "Life isn't about finding yourself. Life is about creating yourself.",
+  },
+  {
+    "author": "Pablo Picasso",
+    "quote":
+      "Every child is an artist. The problem is how to remain an artist once he grows up.",
+  },
+  {
+    "author": "Vincent Van Gogh",
+    "quote": "I am seeking, I am striving, I am in it with all my heart.",
+  },
+  {
+    "author": "C.S. Lewis",
+    "quote": "You are never too old to set another goal or to dream a new dream.",
+  },
+  {
+    "author": "J.R.R. Tolkien",
+    "quote":
+      "All we have to decide is what to do with the time that is given us.",
+  },
+  {
+    "author": "Ernest Hemingway",
+    "quote":
+      "The world breaks everyone, and afterward, some are strong at the broken places.",
+  },
+  {
+    "author": "Maya Angelou",
+    "quote":
+      "I've learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.",
+  },
+  {
+    "author": "Albert Einstein",
+    "quote":
+      "Try not to become a man of success, but rather try to become a man of value.",
+  },
+  {
+    "author": "Nelson Mandela",
+    "quote":
+      "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+  },
+  {
+    "author": "Steve Jobs",
+    "quote":
+      "Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work. And the only way to do great work is to love what you do.",
+  },
+  {
+    "author": "Oprah Winfrey",
+    "quote":
+      "The biggest adventure you can ever take is to live the life of your dreams.",
+  },
+  {
+    "author": "Walt Disney",
+    "quote":
+      "All our dreams can come true, if we have the courage to pursue them.",
+  },
+  {
+    "author": "Mark Twain",
+    "quote": "The secret of getting ahead is getting started.",
+  },
+  {
+    "author": "Eleanor Roosevelt",
+    "quote":
+      "The future belongs to those who believe in the beauty of their dreams.",
+  },
+  {
+    "author": "Abraham Lincoln",
+    "quote":
+      "In the end, it's not the years in your life that count. It's the life in your years.",
+  },
+  {
+    "author": "Martin Luther King Jr.",
+    "quote":
+      "Darkness cannot drive out darkness; only light can do that. Hate cannot drive out hate; only love can do that.",
+  },
+  {
+    "author": "Mother Teresa",
+    "quote":
+      "Spread love everywhere you go. Let no one ever come to you without leaving happier.",
+  },
+  {
+    "author": "John F. Kennedy",
+    "quote":
+      "Ask not what your country can do for you, ask what you can do for your country.",
+  },
+  {
+    "author": "Mahatma Gandhi",
+    "quote": "Be the change that you wish to see in the world.",
+  },
+  {
+    "author": "Stephen Covey",
+    "quote":
+      "The key is not to prioritize what's on your schedule, but to schedule your priorities.",
+  },
+  {
+    "author": "Henry Ford",
+    "quote": "Whether you think you can or you think you can't, you're right.",
+  },
+  {
+    "author": "Theodore Roosevelt",
+    "quote": "Believe you can and you're halfway there.",
+  },
+  {
+    "author": "Zig Ziglar",
+    "quote":
+      "You don't have to be great to start, but you have to start to be great.",
+  },
+  {
+    "author": "J.K. Rowling",
+    "quote":
+      "It is our choices, Harry, that show what we truly are, far more than our abilities.",
+  },
+  {
+    "author": "Stephen Hawking",
+    "quote": "Intelligence is the ability to adapt to change.",
+  },
+  {
+    "author": "Thomas Edison",
+    "quote": "I have not failed. I've just found 10,000 ways that won't work.",
+  },
+  {
+    "author": "Benjamin Franklin",
+    "quote":
+      "Tell me and I forget. Teach me and I remember. Involve me and I learn.",
+  },
+  {
+    "author": "Winston Churchill",
+    "quote":
+      "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+  },
+  {
+    "author": "Ralph Waldo Emerson",
+    "quote":
+      "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.",
+  },
+  {
+    "author": "William Shakespeare",
+    "quote": "All the world's a stage, and all the men and women merely players.",
+  },
+  {
+    "author": "Leo Tolstoy",
+    "quote":
+      "Everyone thinks of changing the world, but no one thinks of changing himself.",
+  },
+  {
+    "author": "Friedrich Nietzsche",
+    "quote": "He who has a why to live can bear almost any how.",
+  },
+  {
+    "author": "George Bernard Shaw",
+    "quote":
+      "Life isn't about finding yourself. Life is about creating yourself.",
+  },
+  {
+    "author": "Pablo Picasso",
+    "quote":
+      "Every child is an artist. The problem is how to remain an artist once he grows up.",
+  },
+  {
+    "author": "Vincent Van Gogh",
+    "quote": "I am seeking, I am striving, I am in it with all my heart.",
+  },
+  {
+    "author": "C.S. Lewis",
+    "quote": "You are never too old to set another goal or to dream a new dream.",
+  },
+  {
+    "author": "J.R.R. Tolkien",
+    "quote":
+      "All we have to decide is what to do with the time that is given us.",
+  },
+  {
+    "author": "Ernest Hemingway",
+    "quote":
+      "The world breaks everyone, and afterward, some are strong at the broken places.",
+  },
+  {
+    "author": "Maya Angelou",
+    "quote":
+      "I've learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.",
+  },
+  {
+    "author": "Albert Einstein",
+    "quote":
+      "Try not to become a man of success, but rather try to become a man of value.",
+  },
+  {
+    "author": "Nelson Mandela",
+    "quote":
+      "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+  },
+  {
+    "author": "Steve Jobs",
+    "quote":
+      "Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work. And the only way to do great work is to love what you do.",
+  },
+  {
+    "author": "Oprah Winfrey",
+    "quote":
+      "The biggest adventure you can ever take is to live the life of your dreams.",
+  },
+  {
+    "author": "Walt Disney",
+    "quote":
+      "All our dreams can come true, if we have the courage to pursue them.",
+  },
+  {
+    "author": "Mark Twain",
+    "quote": "The secret of getting ahead is getting started.",
+  },
+  {
+    "author": "Eleanor Roosevelt",
+    "quote":
+      "The future belongs to those who believe in the beauty of their dreams.",
+  },
+  {
+    "author": "Abraham Lincoln",
+    "quote":
+      "In the end, it's not the years in your life that count. It's the life in your years.",
+  },
+  {
+    "author": "Martin Luther King Jr.",
+    "quote":
+      "Darkness cannot drive out darkness; only light can do that. Hate cannot drive out hate; only love can do that.",
+  },
+  {
+    "author": "Mother Teresa",
+    "quote":
+      "Spread love everywhere you go. Let no one ever come to you without leaving happier.",
+  },
+  {
+    "author": "John F. Kennedy",
+    "quote":
+      "Ask not what your country can do for you, ask what you can do for your country.",
+  },
+  {
+    "author": "Mahatma Gandhi",
+    "quote": "Be the change that you wish to see in the world.",
+  },
+  {
+    "author": "Stephen Covey",
+    "quote":
+      "The key is not to prioritize what's on your schedule, but to schedule your priorities.",
+  },
+  {
+    "author": "Henry Ford",
+    "quote": "Whether you think you can or you think you can't, you're right.",
+  },
+  {
+    "author": "Theodore Roosevelt",
+    "quote": "Believe you can and you're halfway there.",
+  },
+  {
+    "author": "Zig Ziglar",
+    "quote":
+      "You don't have to be great to start, but you have to start to be great.",
+  },
+  {
+    "author": "J.K. Rowling",
+    "quote":
+      "It is our choices, Harry, that show what we truly are, far more than our abilities.",
+  },
+  {
+    "author": "Stephen Hawking",
+    "quote": "Intelligence is the ability to adapt to change.",
+  },
+  {
+    "author": "Thomas Edison",
+    "quote": "I have not failed. I've just found 10,000 ways that won't work.",
+  },
+  {
+    "author": "Benjamin Franklin",
+    "quote":
+      "Tell me and I forget. Teach me and I remember. Involve me and I learn.",
+  },
+  {
+    "author": "Winston Churchill",
+    "quote":
+      "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+  },
+  {
+    "author": "Ralph Waldo Emerson",
+    "quote":
+      "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.",
+  },
+  {
+    "author": "William Shakespeare",
+    "quote": "All the world's a stage, and all the men and women merely players.",
+  },
+  {
+    "author": "Leo Tolstoy",
+    "quote":
+      "Everyone thinks of changing the world, but no one thinks of changing himself.",
+  },
+  {
+    "author": "Friedrich Nietzsche",
+    "quote": "He who has a why to live can bear almost any how.",
+  },
+  {
+    "author": "George Bernard Shaw",
+    "quote":
+      "Life isn't about finding yourself. Life is about creating yourself.",
+  },
+  {
+    "author": "Pablo Picasso",
+    "quote":
+      "Every child is an artist. The problem is how to remain an artist once he grows up.",
+  },
+  {
+    "author": "Vincent Van Gogh",
+    "quote": "I am seeking, I am striving, I am in it with all my heart.",
+  }
+]
+
+function randomSelector(arrayLength) {
+  return Math.floor(Math.random() * arrayLength);
+}
+
+function generateQuote() {
+  var randomNumber = randomSelector(arrayOfQuotes.length);
+  document.getElementById("quoteOutput").innerHTML =
+    '"' + arrayOfQuotes[randomNumber].quote + '"';
+  document.getElementById("authorOutput").innerHTML =
+    "- " + arrayOfQuotes[randomNumber].author;
+}
